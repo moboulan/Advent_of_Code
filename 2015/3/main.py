fec8cab40ec22191a413e962f359b116974f476e
moboulan <01boulanouar@gmail.com>
2026-09-24 19:58:19 +0000

    day 3 part 2

--- 2015/3/main.py
+++ 2015/3/main.py
@@ -2,15 +2,15 @@
 def parse(f_name: str) -> str:
 
     with open(f_name) as f:
-        return f.readline()
+        return f.readline().rstrip('\n')
 
-def solve() -> int:
-    moves = parse("test.txt")
+def get_moves(moves: list[str]):
 
     houses = {(0, 0)}
-
     r, c = (0, 0)
+
     for m in moves:
+
         if m == '>':
             c += 1
         elif m == '<':
@@ -19,9 +19,25 @@ def solve() -> int:
             r -= 1
         elif m == 'v':
             r += 1
+
         houses.add((r, c))
 
-    return len(houses)
+    return houses
+
+
+def solve() -> int:
+    moves = parse("test.txt")
+    santa_moves = []
+    santabot_moves = []
+
+    for i, c in enumerate(moves):
+        if i % 2 == 0:
+            santa_moves.append(c)
+        else:
+            santabot_moves.append(c)
+  
+    return len(get_moves(santa_moves) | get_moves(santabot_moves))
+
 
 def main():
     result = solve()
